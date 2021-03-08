9fcc9ad22f85ac8228042166605f0f4f33d6d928
Sumaya Hassan <sumaya_198@hotmail.co.uk>
2021-03-08 11:44:19 +0000

    working on dtenv

--- server.js
+++ server.js
@@ -15,11 +15,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+const URI = process.env.MONGODB_URI;
+
+mongoose.connect(URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true
-});
+})
+.then(()=> console.log('successfully connected to the database'))
+.catch((err => console.log('Error' + err)));
 
 // routes
 app.use(require("./routes/api.js"));
